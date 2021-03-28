2ad43b53da22d7106c2a1c1a4ec9b511b19d7e68
Girish S <girishten@live.com>
2021-03-28 12:22:40 +0530

    initial setup

---
+++ scripts/gt-radar.js
@@ -0,0 +1,5 @@
+/*!
+ * GT Radar v1.0.0-alpha1 (https://github.com/girishten/gt-radar.git)
+ * Copyright 2021 Girish S (girishten@live.com)
+ * Licensed under MIT (https://github.com/girishten/gt-radar/blob/main/LICENSE)
+ */
